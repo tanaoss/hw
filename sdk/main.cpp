63ae34a186cf80c421b056e024075cdcb891cf38
Ta <1786325243@qq.com>
2023-03-11 22:22:34 +0800

    删除了一些不需要的函数

--- sdk/main.cpp
+++ sdk/main.cpp
@@ -9,16 +9,7 @@ extern vector<Studio> studios;
 extern vector<Robot> robots;
 extern State state;//当前帧数，全局可见
 
-bool readUntilOK() {
-    char line[1024];
-    while (fgets(line, sizeof line, stdin)) {
-        if (line[0] == 'O' && line[1] == 'K') {
-            return true;
-        }
-        //do something
-    }
-    return false;
-}
+
 int main()
 {
     std::ios::sync_with_stdio(false);   
