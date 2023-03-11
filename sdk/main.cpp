c90d19af872ccbbaa0bb9cdadb70ffac9f64ec5e
Ta <1786325243@qq.com>
2023-03-11 11:49:13 +0800

    struct init

--- sdk/main.cpp
+++ sdk/main.cpp
@@ -1,5 +1,51 @@
 #include <iostream>
+#include<vector>
 using namespace std;
+struct  Ins
+{
+    pair<int,double> forward;
+    pair<int,double> rotate;
+    int  buy;
+    int sell;
+    int destroy;
+    Ins(pair<int,double> _forward, pair<int,double> _rotate,int  _buy,int _sell,int _destroy):
+    forward(_forward),rotate(_rotate),buy(_buy),sell(_sell),destroy(_destroy){
+
+    }
+};
+
+struct Robot
+{
+    int id;
+    int target_id;//正在赶往的工作台；
+    int get_id;//手上拿着的货物编号
+    double x;
+    double y;
+    Ins ins;
+    Robot(int _id,int _target_id,int _get_id,double _x,double _y,Ins _ins):
+    id(_id),target_id(_target_id),get_id(_get_id),x(_x),y(_y),ins(_ins){
+
+    }
+};
+
+struct  Studio
+{
+    int id;
+    int r_id;//正在赶来的机器人id
+    double x;
+    double y;
+    int r_time;//剩余生产时间（帧数）
+    int bitSatus;//原材料格状态
+    int pStatus;//产品格状态 
+    Studio(int _id,int _r_id,double _x,double _y,int _r_time,int _bitSatus,int _pStatus):
+    id(_id),r_id(_r_id),x(_x),y(_y),r_time(_r_time),bitSatus(_bitSatus),pStatus(_pStatus)
+    {
+
+    }
+};
+vector<vector<double>>dis;
+vector<Studio> studios;
+vector<Robot> robots;
 
 bool readUntilOK() {
     char line[1024];
